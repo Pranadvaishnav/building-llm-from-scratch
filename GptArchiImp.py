40d6562aded1daefdea934f1d301cc0cebdf27fc
Pranadvaishnav <kazeno1stigma@gmail.com>
2026-02-01 08:34:12 +0530

    feat: implement LayerNorm class for activation normalization

---
+++ GptArchiImp.py
@@ -0,0 +1,32 @@
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,    # Vocabulary size
+    "context_length": 1024, # Context length
+    "emb_dim": 768,         # Embedding dimension
+    "n_heads": 12,          # Number of attention heads
+    "n_layers": 12,         # Number of layers
+    "drop_rate": 0.1,       # Dropout rate
+    "qkv_bias": False       # Query-Key-Value bias
+}
+
+#Step 1: Use a placeholder for TransformerBlock
+#Step 2: Use a placeholder for LayerNorm
+
+import torch
+import torch.nn as nn
+import tiktoken
+from multiheadAttention import MultiHeadAttention
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+    
+

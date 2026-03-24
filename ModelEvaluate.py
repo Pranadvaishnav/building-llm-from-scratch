2cf6f0e1b0e7ad64866629f5fd11d3763caa8c7b
Pranadvaishnav <kazeno1stigma@gmail.com>
2026-03-24 14:14:08 +0530

    feat: add dataset download and text loading logic

--- ModelEvaluate.py
+++ ModelEvaluate.py
@@ -86,3 +86,29 @@ perplexity = torch.exp(loss)
 print(perplexity)
 
 
+file_path = "the-verdict.txt"
+url = "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt"
+
+if not os.path.exists(file_path):
+    with urllib.request.urlopen(url) as response:
+        text_data = response.read().decode('utf-8')
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(text_data)
+else:
+    with open(file_path, "r", encoding="utf-8") as file:
+        text_data = file.read()
+
+# First 100 characters
+print(text_data[:99])
+
+# Last 100 characters
+print(text_data[-99:])
+
+
+total_characters = len(text_data)
+total_tokens = len(tokenizer.encode(text_data))
+
+print("Characters:", total_characters)
+print("Tokens:", total_tokens)
+
+
